d59e53115fef457cecf29f8031cb17c82af7b7e1
Bradley-Krigbaum <bradley.krigbaum@gmail.com>
2020-05-18 21:18:54 -0700

    added files to project

--- index.js
+++ index.js
@@ -12,3 +12,12 @@ I need this code, but don't know where, perhaps should make some middleware, don
 
 Go code!
 */
+
+const app = require('./server.js');
+
+
+const PORT = 4000;
+
+app.listen(PORT, () => {
+    console.log(`bk: index.js: app.listen: \n*** listening on http://localhost:${PORT} ***\n`);
+});
